50daaab851c3374d6591bb4232741ffd372fecca
handofly <handohyeon12@naver.com>
2025-04-01 09:41:09 +0900

    Add files via upload

---
+++ sample/sketch.js
@@ -0,0 +1,12 @@
+function setup(){
+    createCanvas(400, 400);
+    background(200);
+}
+
+function draw(){
+}
+
+function mousePressed(){
+    fill(255,255,255
+    ellipse(mouseX, mouseY, 50, 50);
+}
